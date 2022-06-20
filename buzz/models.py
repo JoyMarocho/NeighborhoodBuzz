eed50a171a36c419b19edbe16c928a6bcdd1deaf
JoyMarocho <joymarocho@gmail.com>
2022-06-20 10:45:35 +0300

    Creating and saving a SuperUser with the given email and password.

--- buzz/models.py
+++ buzz/models.py
@@ -31,12 +31,26 @@ class UserManager(BaseUserManager):
 
 def create_staffuser(self,email,password,**extra_fields):
         """
-        Create and save a Staff User with the given email and password.
+            Create and save a Staff User with the given email and password.
         """
         extra_fields.setdefault('is_staff',True)
         extra_fields.setdefault('is_active',True)
     
         if extra_fields.get('is_staff') is not True:
             raise ValueError('SUpdateUserFormtaff user must have is_staff=True.')
+        return self.create_user(email, password, **extra_fields)
+
+def create_superuser(self,email,password=None,**extra_fields):
+        """
+            Create and save a SuperUser with the given email and password.
+        """
+    #extra_fields.setdefault('is_staff', True)
+    #extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active', True)
+
+        # if extra_fields.get('is_staff') is not True:
+            #   raise ValueError('Superuser must have is_staff=True.')
+        # if extra_fields.get('is_superuser')is not True:
+            #   raise ValueError('Superuser must have superuser=True.')
         return self.create_user(email, password, **extra_fields)
 
