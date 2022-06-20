4e05526250d142e3d747ff3c798f0cf60f8d43c1
JoyMarocho <joymarocho@gmail.com>
2022-06-20 10:18:24 +0300

    creating newuser form

---
+++ buzz/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import Profile,User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+#Your form details here
+class NewUserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ("email", "password1","password2")
+
+    def save(self,commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
