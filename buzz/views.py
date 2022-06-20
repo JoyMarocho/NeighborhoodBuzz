315c4d6e51fdbdf87af909513f2ceb9325ad9a42
JoyMarocho <joymarocho@gmail.com>
2022-06-20 14:13:41 +0300

    creating login function

--- buzz/views.py
+++ buzz/views.py
@@ -27,3 +27,21 @@ def register_user(request):
     form = NewUserForm()
     return render(request, 'registration/registration_form.html', {"registration_form": form})
 
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, "You are now logged in as {username}.")
+                return redirect('homepage')
+            else:
+                messages.error(request, 'Invalid Username or password')
+        else:
+            messages.error(request, 'Invalid username or password')
+    form = AuthenticationForm()
+    return render(request, 'registration/login_form.html', {"login_form": form})
+
